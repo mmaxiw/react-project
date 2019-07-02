360a1797e1ba5eacb0e2dab820baf11d71fc2fed
Maxi Wainberg <m.wainberg@gmail.com>
2019-07-02 17:18:12 +0300

    Updates

---
+++ precache-manifest.1f64f0d950a54984b1ff6b0e3a6ba8d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62434545df90ebf9310ee830f7d7ce3",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "784b26ee0c520ed9cdf2",
+    "url": "/react-project/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c6475d2c0fc5448216b1",
+    "url": "/react-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "784b26ee0c520ed9cdf2",
+    "url": "/react-project/static/js/2.e6e27c1f.chunk.js"
+  },
+  {
+    "revision": "c6475d2c0fc5448216b1",
+    "url": "/react-project/static/js/main.6c460a4e.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  }
+]);
